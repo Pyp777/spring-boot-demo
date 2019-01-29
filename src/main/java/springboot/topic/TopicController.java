37feda17026c5e22d9a5fff2613726b04ef3dc94
Pyp777 <47151241+Pyp777@users.noreply.github.com>
2019-01-29 17:07:39 +0100

    init

---
+++ src/main/java/springboot/topic/TopicController.java
@@ -0,0 +1,25 @@
+package springboot.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@Autowired
+	TopicService service;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return service.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return service.getTopic(id);
+	}
+}
